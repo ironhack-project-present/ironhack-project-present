472667dec98eb13c224085b293c239e9b11ac910
Davide Loprewsti <davidelopresti0129@gmail.com>
2023-01-09 14:57:41 +0000

    second commit

--- app.js
+++ app.js
@@ -33,6 +33,9 @@ app.use("/", indexRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+const presentRoutes = require("./routes/present.routes");
+app.use("/presents", presentRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
